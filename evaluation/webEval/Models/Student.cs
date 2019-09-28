2adbc2f9bef4964e15b2f6464dfb9926d47dd27c
Andreu Loayza <andreu.loayza@hotmail.com>
2019-09-27 22:50:28 -0400

    commit pre controladores

---
+++ evaluation/webEval/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace webEval.Models
+{
+    public class Student
+    {
+        public enum sex
+        {
+            male,
+            female
+        }
+        [Key]
+        public int StudentID { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public sex gender { get; set; }
+        public virtual ICollection<Grade> Grades { get; set; }
+    }
+}
